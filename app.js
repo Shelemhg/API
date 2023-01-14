a365fcd90025ca08d07b93b95f70f72289a324aa
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-01-13 20:44:13 -0600

    Change Mongo_URL from VS Code to API for RENDER

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongodb = require('./db/connect');
 
 
 const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json()).use((req, res, next) => {
