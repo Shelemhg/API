4439a39f4deef6a7b0f7ebe7596a48058077861f
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-01-17 15:10:32 -0600

    Added comments and TODO comments for CRUD func.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,7 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
+// When a GET request is received, this function is executed and it prints to the browser all the contacts in the database.
 const getAll = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
@@ -9,6 +10,7 @@ const getAll = async (req, res, next) => {
   });
 };
 
+// When a GET request is received, this function expects an ID and prints to the web browser all the info of the ID
 const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
@@ -21,6 +23,17 @@ const getSingle = async (req, res, next) => {
     res.status(200).json(lists[0]);
   });
 };
+
+
+// TODO:  const createContact
+
+// TODO: const updateContact
+
+// TODO: const deleteContact
+
+
+
+
 
 module.exports = { getAll, getSingle };
 //FINISHED
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,9 +3,20 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+// Creates a new Router instance and registers a GET request handler for the '/' route. Then the "contactsController.getAll" handler function is passed to the get methig.
+
+//When you receive a get with '/' pass it to contactsController.getAll
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
+
+// TODO:  router.post SOMETHING contactsController.createContact
+
+// TODO: router.put SOMETHING contactsController.updateContact
+
+// TODO: router.delete SOMETHING contactsController.deleteContact
+
+
 
 module.exports = router;
 //FINISHED
