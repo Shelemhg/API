27e1f936ab1977551a8a8f0153d9e58a68709d95
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-01-17 15:16:15 -0600

    Update indentation to 4 spaces on Tab

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+	const express = require('express');
+	const bodyParser = require('body-parser');
+	const mongodb = require('./db/connect');
 
-const port = process.env.PORT || 3000;
+	const port = process.env.PORT || 3000;
 
-const app = express();
+	const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+	app
+	.use(bodyParser.json())
+	.use((req, res, next) => {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		next();
+	})
+	.use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
-});
-  //FINISHED
+	mongodb.initDb((err, mongodb) => {
+	if (err) {
+		console.log(err);
+	} else {
+		app.listen(port);
+		console.log(`Connected to DB and listening on ${port}`);
+	}
+	});
+	//FINISHED
 
 
-// app.get('/', (req, res) => {
-//     res.send('Shelem H.');
-// });
-// const port = process.env.PORT || 8080;
+	// app.get('/', (req, res) => {
+	//     res.send('Shelem H.');
+	// });
+	// const port = process.env.PORT || 8080;
--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,39 +1,45 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+    const mongodb = require('../db/connect');
+    const ObjectId = require('mongodb').ObjectId;
 
-// When a GET request is received, this function is executed and it prints to the browser all the contacts in the database.
-const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
-};
+    // When a GET request is received, this function is executed and it prints to the browser all the contacts in the database.
+    const getAll = async (req, res, next) => {
+    const result = await mongodb.getDb().db().collection('contacts').find();
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+    });
+    };
 
-// When a GET request is received, this function expects an ID and prints to the web browser all the info of the ID
-const getSingle = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
-};
-
-
-// TODO:  const createContact
-
-// TODO: const updateContact
-
-// TODO: const deleteContact
+    // When a GET request is received, this function expects an ID and prints to the web browser all the info of the ID
+    const getSingle = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+        .getDb()
+        .db()
+        .collection('contacts')
+        .find({ _id: userId });
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    });
+    };
 
 
+    // TODO:  const createContact
+
+    const createContact = async (req, res, next) => {
+    
+
+
+    }
+
+    // TODO: const updateContact
+
+    // TODO: const deleteContact
 
 
 
-module.exports = { getAll, getSingle };
-//FINISHED
+
+
+    module.exports = { getAll, getSingle };
+    //FINISHED
